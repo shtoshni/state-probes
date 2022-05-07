6c4c379e4cfb3b3875acdb659c67a912877b251d
shtoshni92 <shtoshni@ttic.edu>
2022-05-07 11:40:16 -0400

    Updates

--- shtoshni_lm/data_transformer.py
+++ shtoshni_lm/data_transformer.py
@@ -9,8 +9,12 @@ from shtoshni_lm.config import PROBE_START, PROBE_END
 
 def identify_beaker_idx(state_targets, subsequent_state_targets):
     output = []
-    for state_target, subsequent_state_target in zip(state_targets, subsequent_state_targets):
-        for idx, (before_content, after_content) in enumerate(zip(state_target, subsequent_state_target)):
+    for state_target, subsequent_state_target in zip(
+        state_targets, subsequent_state_targets
+    ):
+        for idx, (before_content, after_content) in enumerate(
+            zip(state_target, subsequent_state_target)
+        ):
             if before_content.strip() != after_content.strip():
                 output.append(idx)
 
@@ -67,7 +71,9 @@ def convert_to_transformer_batches(
         ]
 
         if randomize_state:
-            state_targets = [state_targets[idx] for idx in random_derangement(len(state_targets))]
+            state_targets = [
+                state_targets[idx] for idx in random_derangement(len(state_targets))
+            ]
         state_targets = {"full_state": state_targets}
 
         # make inputs
@@ -77,7 +83,9 @@ def convert_to_transformer_batches(
         for i, inp in enumerate(inputs):
             string = " ".join(inp).replace(" \n ", ". ")
             steps.append(string)
-            init_state = translate_states_to_nl(init_states[i], domain, isinstance(tokenizer, BartTokenizerFast))
+            init_state = translate_states_to_nl(
+                init_states[i], domain, isinstance(tokenizer, BartTokenizerFast)
+            )
             init_states_str.append(init_state)
             string = init_state + ". " + string
             inps.append(string)
@@ -111,10 +119,14 @@ def convert_to_transformer_batches(
         inp_enc["steps"] = steps
 
         lang_tgt_enc["original_text"] = lang_targets
-        lang_tgt_enc["input_ids"].masked_fill_(lang_tgt_enc["input_ids"] == tokenizer.pad_token_id, -100)
+        lang_tgt_enc["input_ids"].masked_fill_(
+            lang_tgt_enc["input_ids"] == tokenizer.pad_token_id, -100
+        )
         lang_tgt_enc["input_ids"].to(device)
         # print(lang_tgt_enc['input_ids'])
-        beaker_targets = identify_beaker_idx(prev_state_targets, subsequent_state_targets)
+        beaker_targets = identify_beaker_idx(
+            prev_state_targets, subsequent_state_targets
+        )
         for state_key in state_targets:
             state_tgt_enc = None
             if add_state:
@@ -140,7 +152,9 @@ def convert_to_transformer_batches(
                     else:
                         raise ValueError(add_state)
 
-                    target_list.append(represent_add_state_str(state_slice) + lang_target)
+                    target_list.append(
+                        represent_add_state_str(state_slice) + lang_target
+                    )
                     state_slice_list.append(state_slice)
 
                 state_tgt_enc = tokenizer(
@@ -151,7 +165,9 @@ def convert_to_transformer_batches(
                     add_special_tokens=True,
                 ).to(device)
                 state_tgt_enc["state_str"] = state_slice_list
-                state_tgt_enc["input_ids"].masked_fill_(state_tgt_enc["input_ids"] == tokenizer.pad_token_id, -100)
+                state_tgt_enc["input_ids"].masked_fill_(
+                    state_tgt_enc["input_ids"] == tokenizer.pad_token_id, -100
+                )
                 state_tgt_enc["input_ids"].to(device)
 
                 # print(state_tgt_enc['input_ids'])
